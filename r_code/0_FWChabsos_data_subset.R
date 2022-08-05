bbb1bcb735ca393902e06bacbd518cbde8f1daee
Brendan Turley <crabtails@gmail.com>
2022-08-05 09:39:54 -0500

    remove unused dataset

--- r_code/0_FWChabsos_data_subset.R
+++ r_code/0_FWChabsos_data_subset.R
@@ -4,7 +4,6 @@ setwd('~/Desktop/professional/projects/Postdoc_FL/data/habs')
 
 # original data requested from https://habsos.noaa.gov/about
 habs1 <- read.csv('habsos_20220225.csv')
-# habs2 <- read.csv('habsos_proofed_20211210.csv') # unused; older dataset
 
 habs1$date <- dmy_hm(paste(substr(habs1$SAMPLE_DATE,1,15),substr(habs1$SAMPLE_DATE,30,31)))
 
