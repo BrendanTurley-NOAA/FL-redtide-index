fbb7218ddb5c7d248b6687de0d0d09bff751e478
Brendan Turley <crabtails@gmail.com>
2022-08-19 09:43:56 -0500

    finding duplicates

--- r_code/FWC_HABSOS_merge.R
+++ r_code/FWC_HABSOS_merge.R
@@ -1,4 +1,5 @@
 library(lubridate)
+library(scales)
 
 setwd('~/Desktop/professional/projects/Postdoc_FL/data/habs')
 
@@ -13,7 +14,7 @@ habs2 <- read.csv('FL_Kbrevis_1953-2021.03.08.csv')
 names(habs2) <- toupper(names(habs2))
 habs2$date <- mdy(habs2$SAMPLE.DATE)
 habs2 <- habs2[which(year(habs2$date)<2021 & year(habs2$date)>1997),]
-table(habs2$TIME.ZONE,habs2$COUNTY)
+# table(habs2$TIME.ZONE,habs2$COUNTY)
 ### standardize timezones
 switch_data <- function(x){
   renamed <- rep(NA,length(x))
@@ -50,6 +51,7 @@ times <- mdy_hm(paste(habs2$SAMPLE.DATE,habs2$SAMPLE.TIME)) ### assumes everythi
 ### apply local timezone, then convert to UTC
 habs2$date_utc <- force_tzs(times,timez)
 habs2$dataset <- 'FWC'
+### standardize depth column name for merge
 names(habs2)[grep('depth',names(habs2),ignore.case = T)] <- names(habs1)[grep('depth',names(habs1),ignore.case = T)]
 
 
@@ -59,17 +61,54 @@ habs1$LATITUDE <- round(habs1$LATITUDE,5)
 habs1$LONGITUDE <- round(habs1$LONGITUDE,5)
 habs2$LATITUDE <- round(habs2$LATITUDE,5)
 habs2$LONGITUDE <- round(habs2$LONGITUDE,5)
+habs1$SAMPLE_DEPTH <- round(habs1$SAMPLE_DEPTH,3)
+habs2$SAMPLE_DEPTH <- round(habs2$SAMPLE_DEPTH,3)
 habs_merge <- merge(habs1,habs2,by=c('date_utc','LATITUDE','LONGITUDE','SAMPLE_DEPTH'),all=T)
 # habs_overlap <- merge(habs1,habs2,by=c('date_utc','LATITUDE','LONGITUDE','SAMPLE_DEPTH'),all=F)
 overlap <- which(!is.na(habs_merge$dataset.x) & !is.na(habs_merge$dataset.y))
 length(overlap)/nrow(habs_merge)
 habs_overlap <- habs_merge[overlap,]
 ### should be one-to-one
-plot(habs_overlap$CELLCOUNT+1,habs_overlap$KARENIA.BREVIS.ABUNDANCE..CELLS.L.+1,log='xy')
+plot(habs_overlap$CELLCOUNT+1,
+     habs_overlap$KARENIA.BREVIS.ABUNDANCE..CELLS.L.+1,
+     log='xy',pch=16,col=alpha(1,.2))
 habs_overlap$cell_diff <- (habs_overlap$CELLCOUNT-habs_overlap$KARENIA.BREVIS.ABUNDANCE..CELLS.L.)
-length(which(habs_overlap$cell_diff>0))/nrow(habs_overlap)
+length(which(habs_overlap$cell_diff>0 | habs_overlap$cell_diff<0))/nrow(habs_overlap)
 hist(habs_overlap$cell_diff)
-plot(habs_overlap$LONGITUDE,habs_overlap$LATITUDE,asp=1,cex=log(abs(habs_overlap$cell_diff))/5)
+# plot(habs_overlap$LONGITUDE,
+#      habs_overlap$LATITUDE,
+#      asp=1,cex=log(abs(habs_overlap$cell_diff))/5,
+#      pch=16,col=alpha(1,.1))
+par(mfrow=c(1,2))
+plot(habs_overlap$LONGITUDE[which(habs_overlap$cell_diff<0)],
+       habs_overlap$LATITUDE[which(habs_overlap$cell_diff<0)],
+       cex=log(abs(habs_overlap$cell_diff[which(habs_overlap$cell_diff<0)]))/5,
+     asp=1,pch=21,col=alpha(1,.1),bg=alpha('blue',.1))
+plot(habs_overlap$LONGITUDE[which(habs_overlap$cell_diff>0)],
+       habs_overlap$LATITUDE[which(habs_overlap$cell_diff>0)],
+       cex=log(abs(habs_overlap$cell_diff[which(habs_overlap$cell_diff>0)]))/5,
+       asp=1,pch=21,col=alpha(1,.1),bg=alpha('red',.1))
+### these plots are the same; what is going on?
+length(which(habs_overlap$cell_diff>0 | habs_overlap$cell_diff<0))
+rbind(habs_overlap[which.max(habs_overlap$cell_diff),],
+      habs_overlap[which.min(habs_overlap$cell_diff),])
+habs_overlap_diff <- habs_overlap[which(habs_overlap$cell_diff>0 | habs_overlap$cell_diff<0),]
+habs_overlap_diff<- habs_overlap_diff[order(habs_overlap_diff$date_utc,habs_overlap_diff$LATITUDE,habs_overlap_diff$LONGITUDE),]
+plot(habs_overlap_diff$cell_diff[seq(1,nrow(habs_overlap_diff),2)],
+     habs_overlap_diff$cell_diff[seq(2,nrow(habs_overlap_diff),2)])
+
+dups <- which(duplicated(habs_overlap_diff))
+which(duplicated(habs_overlap_diff,fromLast = T))
+habs_overlap_diff <- habs_overlap_diff[-dups,]
+# habs_overlap_diff[127:128,]
+dups1 <- which(duplicated(habs_overlap_diff[,1:3]))
+dups2 <- which(duplicated(habs_overlap_diff[,1:3],fromLast = T))
+reps <- data.frame(dups1, habs_overlap_diff$CELLCOUNT[dups1],
+                   dups2, habs_overlap_diff$KARENIA.BREVIS.ABUNDANCE..CELLS.L.[dups2])
+names(reps) <- c('inx2','habsos_cells','inx2','fwc_cells')
+reps$diff <- reps$habsos_cells-reps$fwc_cells
+inx <- which(reps$diff!=0)
+habs_dups <- habs_overlap_diff[sort(c(reps[inx,1],reps[inx,3])),]
 
 habsos <- which(habs_merge$dataset.x=='HABSOS')
 habsos_r <- which(habs_merge$dataset.x=='HABSOS' & is.na(habs_merge$dataset.y))
